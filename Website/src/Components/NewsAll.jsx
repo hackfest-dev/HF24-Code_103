8037e4f67fb74b839c466524640e097135ed3086
Shivansh-22866 <shivanshp0418@gmail.com>
2024-04-06 11:25:40 +0530

    News card fix

--- Website/src/Components/NewsAll.jsx
+++ Website/src/Components/NewsAll.jsx
@@ -19,15 +19,19 @@ const NewsAll = () => {
       <Navbar />
       <div>
         <h2
-          className="text-center"
+          className="text-center mb-5"
           style={{
-            paddingTop: "80px",
-            paddingBottom: "5px",
-            backgroundColor: "beige",
+            paddingTop: "15px",
+            paddingBottom: "15px",
+            backgroundColor: "#4753AB",
+            color:"white",
+            fontSize: "2rem",
+            fontFamily: "Poppins"
           }}
         >
-          Latest <span className="badge text-bg-info">News</span>
+          Latest <span className="bg-[#71bef1] p-2 rounded-xl text-black">News</span>
         </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {articles.map((news, index) => {
           return (
             <NewsItem
@@ -39,6 +43,7 @@ const NewsAll = () => {
             />
           );
         })}
+        </div>
       </div>
     </>
   );
--- Website/src/Components/NewsItem.jsx
+++ Website/src/Components/NewsItem.jsx
@@ -1,14 +1,14 @@
-
+import Image from '../assets/news-img.png'
 
 const NewsItem = ({title,description,src,url}) => {
     // const { title,description,src,url, ...filteredProps } = this.props;
     return (
-      <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
-              <img src={src?src:Image} style={{height:"200px",width:""}} className="card-img-top" alt="..."/>
+      <div className="text-light mb-3 inline-block m-4 px-2 py-2 bg-[#F1F1F1] rounded-xl shadow-lg" style={{maxWidth:"40rem", height:"25rem"}}>
+              <img src={src?src:Image} style={{height:"200px",width:""}} className="card-img-top mx-auto p-2" alt="..."/>
               <div className="card-body">
-                  <h5 className="card-title">{title.slice(0, 50)}...</h5>
-                  <p className="card-text">{description?description.slice(0, 100):"News happened just recently.It is information about something that has just happened."}</p>
-                  <a href={url} className="btn btn-primary">Read More</a>
+                  <h5 className="p-2 m-1 font-bold font-dosis">{title.slice(0, 50)}...</h5>
+                  <p className="p-2 m-1 font-dosis">{description?description.slice(0, 100):"News happened just recently.It is information about something that has just happened."}</p>
+                  <a href={url} className="p-2 mx-1 my-2 bg-[#4753AB] rounded-xl text-white font-dosis font-bold">Read More</a>
               </div>
           </div>
     )
